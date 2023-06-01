89e5cdbb572871784126f586a32c4d4e6950f4ad
daniel dogadajev <daniel.dogadajev@gmail.com>
2023-06-01 11:14:21 +0300

    Modified MonkeyService

--- MonkeyFinder/Services/MonkeyService.cs
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -1,6 +1,32 @@
-﻿namespace MonkeyFinder.Services
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services
 {
 	public class MonkeyService
 	{
+		HttpClient httpClinet;
+		public MonkeyService()
+		{
+			httpClinet = new HttpClient();
+		}
+
+		List<Monkey> monkeyList = new();
+
+		public async Task<List<Monkey>> GetMonkeys()
+		{
+			if(monkeyList?.Count > 0)
+				return monkeyList;
+
+			var url = "https://montemagno.com/monkeys.json";
+
+			var response = await httpClinet.GetAsync(url);
+
+			if(response.IsSuccessStatusCode)
+			{
+				monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+			}
+
+			return monkeyList;
+		}
 	}
 }
