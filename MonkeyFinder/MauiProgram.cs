d36e400c9bfdcc04f9dc625e0c09b812031d9fcf
daniel dogadajev <daniel.dogadajev@gmail.com>
2023-06-01 11:37:48 +0300

    Modified MauiProgram.cs

--- MonkeyFinder/MauiProgram.cs
+++ MonkeyFinder/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MonkeyFinder.Services;
 using MonkeyFinder.View;
 
 namespace MonkeyFinder;
@@ -15,9 +16,11 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
 
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
+		builder.Services.AddSingleton<MonkeyService>();
+
+		builder.Services.AddSingleton<MonkeysViewModel>();
+
+		builder.Services.AddSingleton<MainPage> ();
 
 		return builder.Build();
 	}
